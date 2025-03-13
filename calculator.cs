93782dfffa24712c101b7789f83305bf235e4c15
Arvexia <muhammadarifin.mukhlis10@students.amikom.ac.id>
2025-03-13 11:32:58 +0700

    update

--- calculator.cs
+++ calculator.cs
@@ -2,7 +2,20 @@ class calculator
 {
 	static word void main ( string [] arga )
 	{
+		int a = 10;
+		int b = 6;
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
 		
+		Console.WriteLine("\nTekan sembarang key untuk keluar");
+		Console.ReadKey();
+}
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
 	}
-
+	static int pengurangan(int a, int b)
+	{
+		return a - b;	
+	}
 }
